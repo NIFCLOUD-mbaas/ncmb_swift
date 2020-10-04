0669280dd62843ea760b7a6b23584d1092d75968
kenta_okamura <side.junktown@gmail.com>
2020-10-04 19:16:50 +0900

    fix CommonCrypto dependency

--- Package.swift
+++ Package.swift
@@ -10,10 +10,27 @@ let package = Package(
     products: [
         .library(name: "NCMB", targets: ["NCMB"]),
     ],
+    dependencies: {
+#if canImport(CommonCrypto)
+        return []
+#else
+        return [
+            .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.1"),
+        ]
+#endif
+    }(),
     targets: [
         .target(
             name: "NCMB",
-            dependencies: [],
+            dependencies: {
+#if canImport(CommonCrypto)
+                return []
+#else
+                return [
+                    .product(name: "CryptoSwift", package: "CryptoSwift"),
+                ]
+#endif
+            }(),
             path: "NCMB"
         ),
         .testTarget(
