7e0960da78a00f8bda167f198001d6b6e36e3a0b
dinh.thuy.duong <dinh.thuy.duong@fujitsu.com>
2022-11-22 15:30:29 +0900

    Update support platforms version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "NCMB",
     platforms: [
-        .iOS(.v15), .macOS(.v12)
+        .iOS(.v13), .macOS(.v11)
     ],
     products: [
         .library(name: "NCMB", targets: ["NCMB"]),
